98e3938ae4f83b811038a4a6cc747b587a1dbcb3
Ubuntu <ubuntu@ip-172-31-19-154.us-west-2.compute.internal>
2017-11-13 03:15:25 +0000

    added scripts and demo

---
+++ curr_eval_command.sh
@@ -0,0 +1,3 @@
+MODEL=logs/squeezedet_plus_ttq_two_layers
+NET=squeezeDet+
+python2 ./src/eval.py --dataset=KITTI --data_path=./data/KITTI --image_set=val --eval_dir="$MODEL/eval" --checkpoint_path="$MODEL/train" --net=$NET --gpu=0
