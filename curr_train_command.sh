846375e495e31c4d1cde99587c9d347e79f6998e
Brandon Lin <blin00@gmail.com>
2017-11-20 01:53:56 -0800

    make scripts executable

--- curr_train_command.sh
+++ curr_train_command.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 python2 ./src/train.py   --dataset=KITTI   --data_path=./data/KITTI   --image_set=train   --train_dir="logs/squeezedet_plus_ttq_two_layers/train"   --net=squeezeDet+ --pretrained_model_path=./data/SqueezeNet/squeezenet_v1.0_SR_0.750.pkl  --summary_step=100   --checkpoint_step=500   --gpu=0  --max_steps=15000
